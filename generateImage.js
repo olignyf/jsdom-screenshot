92faf71cb1f8bd703e9d011bce971827440f463c
Francois <frank.quebec@gmail.com>
2021-06-18 11:43:03 -0400

    cleanup console.log https://github.com/dferber90/jsdom-screenshot/issues/22

--- generateImage.js
+++ generateImage.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 const merge = require("lodash.merge");
 const debug = require("./debug");
 const { getMergedOptions } = require("./options");
-const fs = require('fs');
 
 // starts a server and returns it
 // - server runs on random open port
@@ -21,8 +20,7 @@ const createServer = async (html, { serve }) => {
       var done = finalhandler(request, response);
       if (request.url === "/") {
         response.setHeader('Content-Type', 'text/html; charset=utf-8');
-        response.end(html);
-        console.log('server response to request with HTML');       
+        response.end(html);    
       }
       done();
     }
@@ -52,9 +50,8 @@ const createServer = async (html, { serve }) => {
       });
       // 0 assigns a random port.
       // The port may be used, so we have to retry to find an unused port
-      console.log('server listen start');
       server.listen(0, (err) => {
-        console.log('server listen returned with err', err);
+        console.error('server listen returned with err', err);
         return (err ? reject(err) : resolve());
       });
     };
@@ -75,20 +72,16 @@ const takeScreenshot = async (url, opts) => {
     // Options see:
     // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions
     const browser = await puppeteer.launch(opts.launch);
-    console.log('puppeteer launch');
     const page = await browser.newPage();
-    console.log('puppeteer newPage');
 
     if (typeof opts.intercept === "function") {
       await page.setRequestInterception(true);
       page.on("request", opts.intercept);
     }
-    //console.log('going to url', url, opts.waitUntilNetworkIdle);
     await page.goto(
       url,
       opts.waitUntilNetworkIdle ? { waitUntil: "networkidle0" } : {}
     );
-    //console.log('after page.goto', url);
 
     // If user provided options.targetSelector we try to find that element and
     // use its bounding box to clip the screenshot.
@@ -113,11 +106,10 @@ const takeScreenshot = async (url, opts) => {
         screenshotOptions.clip
       );
     }
-console.log('going to take screenshot');
-    const image = await page.screenshot(screenshotOptions);
-    console.log("after getting screenshot");
+
+    const image = await page.screenshot(screenshotOptions);    
     browser.close();
-    console.log("after browser close");
+    
     return image; // NEW
   } catch (e) {
     console.error('ERROR: exception during takeScreenshot', e);
@@ -149,15 +141,10 @@ const generateImage = async options => {
   // And we would not be able to serve local assets (the ones included through
   // tags like <img src="/party-parrot.gif" />). We run the node server instead
   // to serve the generated html and to serve local assets.
-  console.log('html', html.substr(0, 100));
-  fs.writeFileSync('c:/dev/decoders.html', html, { encoding: 'utf8'} );
-  console.log('after file write sync');
   const server = await createServer(html, opts);
   const url = `http://localhost:${server.address().port}`;
   const screenshot = await takeScreenshot(url, opts);
-  console.log('after await takeScreenshot');
   await new Promise((resolve) => { 
-    console.log('closing server', server);
     server.close(resolve);
   });
   return screenshot;
